7cfcfa2de48289eb29287a467fc3c5e75f4cc25a
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-21 01:13:48 +0530

    graphql fixes

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,7 @@ from .schema import schema
 from .views import *
 
 urlpatterns = [
-    # re_path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-    re_path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    re_path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
     path('', index),
     path('csv', parse_csv),
 ]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
 
 from app.core.clients.StatisticClient import StatisticClient
-from app.models import CovidStatistics, Country, States
 
 
 def index(request):
-    # countries = Country.objects.all()
-    # for country in countries:
-    #     print(country.name)
-
     with StatisticClient() as statistics_client:
         query = '{ statistics { name flag latitude longitude statistics { confirmed deaths recovered date } } }'
         payload = {"query": query}
         response = statistics_client.get_covid_statistics(body=payload).obj()
-        # print(response.data.statistics.edges)
         context = {
             "statistics": response.data.statistics
         }
