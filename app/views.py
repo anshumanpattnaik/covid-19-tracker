a2a52f3e23718b92f1bf33af419694014176b4b3
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-29 03:40:07 +0530

    graphql query formatting

--- app/views.py
+++ app/views.py
@@ -8,11 +8,13 @@ from app.models import DateConfig
 def index(request):
     with StatisticClient() as statistics_client:
         date = DateConfig.objects.all()[0].date
-        payload = {"query": Utils.graphql_query(date=date)}
+        query = Utils.graphql_query(date=date)
+        payload = {"query": query}
         response = statistics_client.get_covid_statistics(body=payload).obj()
         context = {
             "total_cases": response.data.totalCases.edges[0].node,
             "statistics": response.data.statistics,
-            "date": date
+            "date": date,
+            "graphql_query": query
         }
     return render(request, 'index.html', context)
