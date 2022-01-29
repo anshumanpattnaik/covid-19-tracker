b897b913a5f08e92545c1592e6fd3701bf281daa
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-01-29 20:49:38 +0530

    Select menu stylin

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from app.core.clients.StatisticClient import StatisticClient
 from app.core.utils import Utils
-from app.models import DateConfig
+from app.models import DateConfig, TotalCases
 
 
 def index(request):
@@ -11,10 +11,16 @@ def index(request):
         query = Utils.graphql_query(date=date)
         payload = {"query": query}
         response = statistics_client.get_covid_statistics(body=payload).obj()
+
+        total_cases = TotalCases.objects.all()
+        dates = []
+        for total in total_cases:
+            dates.append(total.date)
         context = {
             "total_cases": response.data.totalCases.edges[0].node,
             "statistics": response.data.statistics,
             "date": date,
+            "all_dates": dates,
             "graphql_query": query
         }
     return render(request, 'index.html', context)
