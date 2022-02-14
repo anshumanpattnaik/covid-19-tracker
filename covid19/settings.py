e1de96a0b8b31c5398ad651d70ab59f12e6bdf6e
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-02-14 17:59:18 +0530

    Dev ENV Setup

--- covid19/settings.py
+++ covid19/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR, '.env.prod'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
