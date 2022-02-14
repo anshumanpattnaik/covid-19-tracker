3002ad57ca2d8bdab7a6b665684c3c3a62c7d5d9
anshumanpattnaik <anshuman.pattnaik524@gmail.com>
2022-02-14 23:02:48 +0530

    Bug Fixes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import requests
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
@@ -13,7 +14,8 @@ from app.serializers import TotalCasesSerializer, CountrySerializer
 def index(request):
     total_cases = TotalCases.objects.all()
     date = total_cases.last().date
-    response = GetStatisticsByDate.get(request=request, date=date).data
+    response = requests.get(f"https://covid19tracker.info/statistics/{date}")
+    # response = GetStatisticsByDate.get(request=request, date=date).data
     dates = []
     for total in total_cases:
         dates.append(total.date)
